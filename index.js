e0ddbef9df85de21dcbd7105d7d15e2bc8d59733
Emily Rizo <emilyrizo@gmail.com>
2024-08-13 00:36:52 -0300

    Inserting Browser by Dress Style and Our Happy Customers

--- index.js
+++ index.js
@@ -1,9 +1,8 @@
 function closeTopBar() {
   const topBar = document.getElementById('topbar-container');
-  topBar.remove();
-  
-  const menuContainer = document.getElementById('menu-container');
-  menuContainer.style.marginTop = '24px';
+  topBar.style.visibility = 'hidden';
+
+  document.querySelector('header').classList.add('no-topbar');
 }
 
 document.addEventListener("DOMContentLoaded", function() {
@@ -14,24 +13,59 @@ document.addEventListener("DOMContentLoaded", function() {
       const fullStars = Math.floor(rating);
       const halfStar = rating % 1 !== 0;
 
-      // Adiciona estrelas inteiras
       for (let i = 0; i < fullStars; i++) {
           const star = document.createElement('img');
-          star.src = './assets/images/products/fullStars.svg'; // Substitua pelo caminho correto
+          star.src = './assets/images/products/fullStars.svg';
           star.alt = 'Star';
           ratingElement.appendChild(star);
       }
 
-      // Adiciona meia estrela se necessário
       if (halfStar) {
           const halfStarImage = document.createElement('img');
-          halfStarImage.src = './assets/images/products/halfStar.svg'; // Substitua pelo caminho correto
+          halfStarImage.src = './assets/images/products/halfStar.svg';
           halfStarImage.alt = 'Half Star';
           ratingElement.appendChild(halfStarImage);
       }
 
-      // Atualiza a nota
-      const ratingScore = ratingElement.nextElementSibling; // Nota vem após as estrelas
+      const ratingScore = ratingElement.nextElementSibling;
       ratingScore.textContent = rating.toFixed(1);
   });
 });
+
+const carousel = document.querySelector('.carousel');
+const carouselTrack = document.querySelector('.carousel-track');
+const prevBtn = document.querySelector('.next-btn'); 
+const nextBtn = document.querySelector('.prev-btn');
+const carouselItems = document.querySelectorAll('.carousel-item');
+
+let currentIndex = 0;
+const itemWidth = carouselItems[0].clientWidth;
+
+const updateCarousel = () => {
+  carouselTrack.style.transform = `translateX(-${currentIndex * itemWidth}px)`;
+
+  carouselItems.forEach(item => {
+    item.style.filter = 'none';
+  });
+  carouselItems[currentIndex].style.filter = 'blur(2px)';
+  if (currentIndex === 0) {
+    carouselItems[carouselItems.length - 1].style.filter = 'blur(2px)';
+  } else if (currentIndex === carouselItems.length - 1) {
+    carouselItems[0].style.filter = 'none';
+  }
+};
+
+prevBtn.addEventListener('click', () => {
+  currentIndex = (currentIndex - 1 + carouselItems.length) % carouselItems.length;
+  updateCarousel();
+});
+
+nextBtn.addEventListener('click', () => {
+  currentIndex = (currentIndex + 1) % carouselItems.length;
+  updateCarousel();   
+
+});
+
+updateCarousel();
+
+
